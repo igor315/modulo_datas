10f1ed8b13b4e5b3f42d3b7f02a4d04718fbed06
Igor Santos Oliveira <iigor315@hotmail.com>
2021-09-23 08:17:11 -0300

    Commit

--- Datas_em_Java/src/DatasEmJava.java
+++ Datas_em_Java/src/DatasEmJava.java
@@ -1,8 +1,10 @@
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class DatasEmJava {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
 		
 		Date date = new Date();
 		
@@ -19,5 +21,24 @@ public class DatasEmJava {
 		System.out.println("Segundos: " + date.getSeconds());
 		
 		System.out.println("Ano: " + (date.getYear() + 1900 ));
+		
+		
+		/*----------Simple Date Format----------*/
+		
+		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm.ss");
+		
+		System.out.println("Data atual em formato padrao e String: " + simpleDateFormat.format(date));
+		
+		simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm.ss");
+		
+		System.out.println("Data em formato para banco de dados: " + simpleDateFormat.format(date));
+		
+		System.out.println("Objeto Date: " + simpleDateFormat.parse("1987-10-18 19:57.50"));
+		
+		simpleDateFormat = new SimpleDateFormat("yyyy-dd-MM");
+		System.out.println("Objeto Date: " + simpleDateFormat.parse("1987-18-10"));
+		
+		simpleDateFormat = new SimpleDateFormat("yyyy/dd/MM");
+		System.out.println("Objeto Date: " + simpleDateFormat.parse("1987/18/10"));
 	}
 }
